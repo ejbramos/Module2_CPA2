34971c6b866e1829bb13036bf6f4f366abe7c760
ejbramos <113459961+ejbramos@users.noreply.github.com>
2023-02-06 16:56:00 +0800

    Add files via upload

---
+++ src/operations.java
@@ -0,0 +1,40 @@
+class operations {
+        public boolean not(boolean p){
+            return (!p);
+    }
+
+    public boolean and(boolean p, boolean q){
+            return p&&q;
+        }
+
+        public boolean or(boolean p, boolean q) {
+        return p||q;
+        }
+
+         public boolean exOr(boolean p, boolean q){
+
+        return (p !=q);
+         }
+
+         public boolean nand(boolean p, boolean q){
+        return !(p&&q);
+         }
+
+         public boolean nor(boolean p, boolean q){
+        return !(p||q);
+         }
+
+         public boolean then(boolean p, boolean q){
+        if (q == true){
+            return true;
+        }
+        if (p == false) {
+            return true;
+        }
+        return false;
+         }
+
+         public boolean biCon(boolean p, boolean q){
+        return (p == q);
+         }
+}
