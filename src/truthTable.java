3977a650d6bd2e4b188108de2051b1cfd566a28b
ejbramos <113459961+ejbramos@users.noreply.github.com>
2023-02-06 16:51:17 +0800

    Add files via upload

--- src/truthTable.java
+++ src/truthTable.java
@@ -7,9 +7,29 @@ public class truthTable {
         Scanner input = new Scanner(System.in);
         System.out.println("Input Expression: ");
         String inputted = input.next();
-        System.out.println(inputted);
+        System.out.println(toInfix(inputted));
 
+    }
+    private static Object toInfix(String infix)
+//converts an infix expression to postfix
+    {
+        MyStack stack = new MyStack();
+        char symbol;
 
+        for (int i = 0; i < infix.length();i++){
+            symbol = infix.charAt(i);
 
+            if(Character.isLetter(symbol)){
+                stack.push(symbol);
+            }
+            else if(symbol == '¬'){
+                String temp = stack.top().toString();
+                stack.pop();
+
+                stack.push(symbol+temp);
+            }
+
+        }
+    return stack.top();
     }
 }
